30c54c498c2a630a9f0020c5c55011f9392c7e1f
franciscoaero <leandrosoaresaero@gmail.com>
2024-04-20 19:09:24 -0300

    fix bug in mainV2

--- FILES/api/main.py
+++ FILES/api/main.py
@@ -7,7 +7,9 @@ import tensorflow as tf
 
 app = FastAPI()
 
-MODEL = tf.keras.models.load_model("../models/plant_disease_model_v1.keras", custom_objects=None, compile=True, safe_mode=True)
+MODEL = tf.keras.models.load_model("FILES/models/plant_disease_model_v1.keras")
+
+
 CLASS_NAMES = ["Early Blight", "Late Blight", "Healthy"]
 
 def read_file_as_image(data) -> np.ndarray:
@@ -22,7 +24,16 @@ async def predict(
     img_batch = np.expand_dims(image, 0)
 
     prediction = MODEL.predict(img_batch)
+    
+    prediceted_class = CLASS_NAMES[np.argmax(prediction[0])]
+    confidence = np.max(prediction[0])
     pass
+'''
+    return {
+        "class": prediction, 
+        "confidence": float(confidence)
+    }
+'''
 
 if __name__ == "__main__":
     uvicorn.run(app, host="localhost", port=8585)
--- FILES/training/teste.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import tensorflow as tf
-#from keras.preprocessing.image import ImageDataGenerator
-import matplotlib.pyplot as plt
-
-model = tf.keras.models.load_model("FILES/training/trained_model.keras")
-
-model.summary()
